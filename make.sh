975bbcc96b729c20cd852777ab574036a0f50382
Kolya <n.krat@carbonsoft.ru>
2017-10-18 00:24:54 +0300

    Opencarbon start

--- make.sh
+++ make.sh
@@ -2,5 +2,11 @@
 
 mkdir -p ./opencarbon_kolko
 for page in ./data/blog/*; do
-    cp -a "$page" ./opencarbon_kolko/${page##*/}
+    cp -a "$page" ./opencarbon_kolko/${page##*/}.txt
+    echo '{{indexmenu_n>1000}}' > ./opencarbon_kolko/start.txt
+done
+
+echo "<html><body><h1>Статьи:</h1>" > ./index.html
+for page in ./data/blog/*; do
+    echo "<p><a href='https://kolko.github.io/data/blog/${page##*/}'>${page##*/}</a>" >> ./index.html
 done
---
+++ up.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd /root/kolko.github.io
+git pull origin master
+chown -R apache:apache /root/kolko.github.io/opencarbon_kolko/
+for f in /root/kolko.github.io/opencarbon_kolko/*; do
+ cp -a $f /var/www/html/data/pages/участники/kolko/${f##*/}.txt
+done
+

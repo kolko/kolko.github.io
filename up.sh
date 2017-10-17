4f546b381f95c9aeca7ff06429f7deb1001e1652
Kolya <n.krat@carbonsoft.ru>
2017-10-18 00:45:23 +0300

    Fix opencarbon

--- up.sh
+++ up.sh
@@ -4,6 +4,6 @@ cd /root/kolko.github.io
 git pull origin master
 chown -R apache:apache /root/kolko.github.io/opencarbon_kolko/
 for f in /root/kolko.github.io/opencarbon_kolko/*; do
- cp -a $f /var/www/html/data/pages/участники/kolko/${f##*/}.txt
+ cp -a $f /var/www/html/data/pages/участники/kolko/${f##*/}
 done
 

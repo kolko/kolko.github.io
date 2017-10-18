1df6d526f48346e04e750cbdf9c0ba71510c04e9
Kolya <n.krat@carbonsoft.ru>
2017-10-18 11:11:45 +0300

    Do not read mans lol

--- make.sh
+++ make.sh
@@ -6,13 +6,13 @@ for page in ./data/blog/*; do
     echo '{{indexmenu_n>1000}}' > ./opencarbon_kolko/start.txt
 done
 
-mkdir -p ./kolko.github.io
+mkdir -p ./githubio
 echo "<html><body><h1>Статьи:</h1>" > ./index.html
 for page in ./data/blog/*; do
-    echo "<p><a href='https://kolko.github.io/kolko.github.io/${page##*/}.html'>${page##*/}</a>" >> ./index.html
+    echo "<p><a href='https://kolko.github.io/githubio/${page##*/}.html'>${page##*/}</a>" >> ./index.html
 
-    echo '<html><body><script src="/showdown.min.js"></script><textarea id="sourceTA" style="display: none">' > ./kolko.github.io/${page##*/}.html
-    cat $page >> ./kolko.github.io/${page##*/}.html
+    echo '<html><body><script src="/showdown.min.js"></script><textarea id="sourceTA" style="display: none">' > ./githubio/${page##*/}.html
+    cat $page >> ./githubio/${page##*/}.html
     echo "</textarea><div id='targetDiv'></div>
     <script>
   var text = document.getElementById('sourceTA').value,
@@ -23,5 +23,5 @@ for page in ./data/blog/*; do
     target.innerHTML = html;
 </script>
 </body>
-</html>" >> ./kolko.github.io/${page##*/}.html
+</html>" >> ./githubio/${page##*/}.html
 done

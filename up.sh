645887ff01283b6ad24595ab2706d76434d03863
Kolya <n.krat@carbonsoft.ru>
2019-03-14 01:54:29 +0300

    Update up.sh

--- up.sh
+++ up.sh
@@ -3,8 +3,8 @@
 cd /root/kolko.github.io
 git pull origin master
 chown -R apache:apache /root/kolko.github.io/opencarbon_kolko/
-rm -f /var/www/html/data/pages/участники/kolko/*
+#rm -f /var/www/html/data/pages/участники/kolko/*
 for f in /root/kolko.github.io/opencarbon_kolko/*; do
- cp -a $f /var/www/html/data/pages/участники/kolko/${f##*/}
+ cp -a $f /var/www/html/data/pages/блоги_и_отзывы/krat_nikolay/${f##*/}
 done
 

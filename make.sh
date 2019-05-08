57e0999166b84e3ebb67a128f76ff3fb4bf35fad
Kolya <n.krat@carbonsoft.ru>
2019-05-08 20:31:22 +0300

    Add yandex metrica

--- make.sh
+++ make.sh
@@ -8,13 +8,49 @@ for page in ./data/blog/*; do
 done
 
 mkdir -p ./githubio
-echo "<html><body>
+echo "<html>
+<head>
+<!-- Yandex.Metrika counter -->
+<script type='text/javascript' >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+   ym(53586970, 'init', {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src='https://mc.yandex.ru/watch/53586970' style='position:absolute; left:-9999px;' alt='' /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+</head>
+<body>
 <p>Есть <a href='http://opencarbon.ru/блоги_и_отзывы:krat_nikolay:start'>филиал</a></p>
 <h1>Статьи:</h1>" > ./index.html
 for page in $( ls ./data/blog/ | sort -r); do
     echo "<p><a href='/githubio/${page##*/}.html'>${page##*/}</a>" >> ./index.html
 
-    echo '<html><body><script src="/showdown.min.js"></script><textarea id="sourceTA" style="display: none">' > ./githubio/${page##*/}.html
+    echo "<html><head>
+    <!-- Yandex.Metrika counter -->
+<script type='text/javascript' >
+   (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+   m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+   (window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym');
+
+   ym(53586970, 'init', {
+        clickmap:true,
+        trackLinks:true,
+        accurateTrackBounce:true,
+        webvisor:true
+   });
+</script>
+<noscript><div><img src='https://mc.yandex.ru/watch/53586970' style='position:absolute; left:-9999px;' alt='' /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+    </head>" > ./githubio/${page##*/}.html
+
+    echo '<body><script src="/showdown.min.js"></script><textarea id="sourceTA" style="display: none">' >> ./githubio/${page##*/}.html
     cat ./data/blog/${page} >> ./githubio/${page##*/}.html
     echo "</textarea><div id='targetDiv'></div>
     <script>
@@ -29,5 +65,5 @@ for page in $( ls ./data/blog/ | sort -r); do
 </html>" >> ./githubio/${page##*/}.html
 done
 
-git add ./githubio
-git add ./opencarbon_kolko
+#git add ./githubio
+#git add ./opencarbon_kolko
